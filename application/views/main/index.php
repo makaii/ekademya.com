4ab4c1c44ab33e9412b4d5156b0a0f26c9ba4a24
makaii <markjoshuaevangelista@gmail.com>
2017-12-08 20:46:20 +0800

    3rd update
    did somw work on the landing page

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -11,9 +11,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/custom-style.css'); ?>">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand" href="<?php echo base_url(); ?>">E K A D E M Y A</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -40,63 +41,83 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </li>
           <li>
             <button class="btn btn-outline-primary">Become an Instructor</button>
-            <a href="#username"><button class="btn btn-outline-secondary">Signin</button></a>
+            <a href="<?php echo base_url('signin'); ?>"><button class="btn btn-outline-secondary">Signin</button></a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search for Course" aria-label="Search">
+          <input class="form-control mr-sm-2" type="search" placeholder="Explore Courses" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
       </div>
     </nav>
 
-    <div class="jumbotron">
-      <div class="container">
-        <div class="display-4">Ekademya</div>
-        <p>Learn new things everyday, anywhere, anytime</p>
+    <div class="jumbotron jumbotron-header" style="padding: 8em;">
+      <div class="container text-center">
+        <div class="display-4">Classes by expert practitioners</div>
+        <p class="lead" style="margin-top: 1rem;">Learn new things anywhere, anytime</p>
         <div>
-          <button class="btn btn-primary">FAQ</button>
-          <button class="btn btn-info">Why Ekademya?</button>
+          <a href="<?php echo base_url('signup'); ?>"><button class="btn btn-info">Get Started for Free</button></a>
         </div>
       </div>
     </div>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-4">
+    <div class="jumbotron bg-white" style="">
+      <div class="container">
+        <h4 class="text-center">Access our Classes</h4>
+      </div>
+    </div>
+
+    <div class="jumbotron">
+      <div class="container text-center">
+        <p class="display-4">Why Ekademya?</p>
+        <hr>
+        <div class="card-deck">
           <div class="card">
-            <?php echo form_open(base_url('signin')); ?>
-              <div class="card-header bg-secondary text-white">
-                <h6>Sign in to your Ekademya Account!</h6>
-              </div>
-              <div class="card-body bg-light">
-                <div class="form-group">
-                  <label>Email</label>
-                  <input class="form-control form-control-sm" type="text" placeholder="abc@domain.com" name="email" value="<?php echo set_value('email'); ?>"></input>
-                  <?php echo form_error('email'); ?>
-                </div>
-                <div class="form-group">
-                  <label>Password</label>
-                  <input class="form-control form-control-sm" type="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;" name="password" value="<?php echo set_value('password'); ?>"></input>
-                  <?php echo form_error('password'); ?>
-                </div>
-                <!-- <div class="form-check">
-                  <label class="form-check-label text-muted">
-                  <input type="checkbox" class="form-check-input"><small>remember me</small></label>
-                </div> -->
-                <button class="btn btn-outline-primary btn-block" type="submit">Signin</button>
-                <button class="btn btn-link btn-block" type="reset">or Forgot Password</button>
-              </div>
-              <div class="card-footer text-center">
-                <small>Need an account? <strong><a href="<?php echo base_url('signup'); ?>">Create one here</a></strong></small>
-              </div>
-            <?php echo form_close(); ?>
+            <div class="card-body">
+              <h4 class="card-title">Lorem</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut metus eu ligula commodo semper. Integer id leo sollicitudin, ullamcorper.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Lorem</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim fermentum nunc vitae tristique. In ac magna hendrerit.
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Lorem</h4>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat finibus accumsan. Aliquam erat volutpat.
+              </p>
+            </div>
           </div>
         </div>
-        <div class="col-4">
-        </div>
-        <div class="col-4">
-        </div>
+        <!-- <div class="row">
+          <div class="col">
+            <h6>Lorem</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut metus eu ligula commodo semper. Integer id leo sollicitudin, ullamcorper.</p>
+          </div>
+          <div class="col">
+            <h6>Lorem</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim fermentum nunc vitae tristique. In ac magna hendrerit.</p>
+          </div>
+          <div class="col">
+            <h6>Lorem</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat finibus accumsan. Aliquam erat volutpat.</p>
+          </div>
+        </div> -->
+      </div>
+    </div>
+
+    <div class="jumbotron" style="padding: 10em;">
+      <div class="container text-center">
+        <h4>Teach on Ekademya</h4>
+        <p class="lead">Earn money. Share your expertise. Build your personal brand.</p>
       </div>
     </div>
 
